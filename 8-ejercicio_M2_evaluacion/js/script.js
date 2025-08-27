24d01803610ec2c576edf2396fa2596c029baab8
pedro-donoso <pedro.eduardo.donoso@gmail.com>
2025-08-26 21:56:27 -0400

    agrego script menu

--- 8-ejercicio_M2_evaluacion/js/script.js
+++ 8-ejercicio_M2_evaluacion/js/script.js
@@ -1,4 +1,4 @@
-function showSection(sectionName) {
+function showSection(sectionName, clickedElement) {
     const sections = document.querySelectorAll('.section');
     sections.forEach(section => {
         section.classList.add('hidden');
@@ -12,5 +12,21 @@ function showSection(sectionName) {
         link.classList.add('hover:bg-cyan-500');
     });
 
-    
+    if (clickedElement) {
+        clickedElement.classList.add('bg-red-500');
+        clickedElement.classList.remove('hover:bg-cyan-500');
+    }
+
+    document.getElementById('mobile-menu').classList.add('hidden');
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('mobile-menu-button').addEventListener('click', function() {
+        const mobileMenu = document.getElementById('mobile-menu');
+        mobileMenu.classList.toggle('hidden');
+    });
+
+    
+
+
+});
